f9e4280faef37579b35ad6073de466979b812fc6
Brice <brice@gizra.com>
2014-05-19 11:13:29 +0300

    remove keu

--- nodejs.config.js
+++ nodejs.config.js
@@ -3,7 +3,7 @@ settings = {
   port: process.env.PORT || 5000,
   host: '',
   resource: '/socket.io',
-  serviceKey: '8383cn83un4d83un4d83',
+  serviceKey: '',
   backend: {
     host: 'http://new.kedem-auctions.com',
     port: 80,
